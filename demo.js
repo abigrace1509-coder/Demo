6b47bd92cd21269271b701b5e3e21084782868d3
abigrace1509-coder <abigrace1509@gmail.com>
2025-11-27 14:38:36 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,5 @@
+// console.log("Good morning");
+
+// alert("hello world");
+// prompt('How are you?');
+// confirm("Are you okay")
---
+++ looping.js
@@ -0,0 +1,52 @@
+// for(let i=51;i<=100;i++){
+//     console.log("Number:"+i);
+// }
+
+// var x=1;
+// while (x<=3) {
+//     console.log("Welcome");
+//     document.write("hello")
+//     alert("Friends....")
+//     x++;
+// }
+
+// var y=11;
+// do {
+//     console.log(y);
+//     y++;
+// } while (y<=70);
+
+// let fruits=["abi",'prince','priya'];
+// for (const item of fruits) {
+//    console.log(item); 
+// }
+
+// let input={
+//     name:'deva',
+//     age:27,
+//     phone:752863231
+// }
+// console.log(input);
+
+// for(let key in input){
+// console.log(key+":"+input[key]);
+// }
+
+// let num = prompt("Enter a number : ");
+// if (num > 0) {
+//   console.log("The number is Positive");
+// } 
+// else if (num < 0) {
+//   console.log("The number is Negative");
+// } 
+// else {
+//   console.log("The number is Zero");
+// }
+
+// let nums;
+// do {
+//   nums = prompt("Enter a number (enter 0 to stop): ");
+// } while (nums != 0);
+// console.log("Program ended.");
+
+
